143973a4e39f824d1d3dddd4cba1a09b5dc41fd7
MythMetal <wilsonrodri3000@gmail.com>
2025-04-02 10:46:53 -0500

    Finalizacion del primer programa

---
+++ src/main/java/com/mycompany/clase_3/Clase_3.java
@@ -0,0 +1,96 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.clase_3;
+
+/**
+ *
+ * @JosueR Mythmetal
+ */
+public class Clase_3 {
+
+    public static void main(String[] args) {
+        /* = Asignar
+           == Comparar
+         */
+ /*   int contador = 5;
+        while (contador <5 ) {
+            System.out.println("Numero: " + contador);
+          //  contador = contador + 4;
+          //contador++;
+
+     int contador = 5;
+        do {            
+            int contador = 5;
+         {
+            System.out.println("Numero: " + contador);
+          //  contador = contador + 4;
+          contador++;
+        } while (contador <5 ) {
+         */
+        //Escribe el area de un rectangulo.
+        //Determina el ancho y la altura del rectangulo
+       /* float altura = 10;
+        float ancho = 5;
+
+        double area = 0;
+
+        area = ancho * altura;
+        System.out.println("La altura del rectangulo es: " + area);
+        */
+//Determina un numero entero e Imprime si es positivo
+
+/*int numero=-2;
+if(numero>=0){
+    System.out.println("El numero es positivo: "+ numero);
+    }else{
+    System.out.println("El numero es negativo: "+ numero);
+}
+
+ */
+//Escribe un programa que determine si un numero es par o impar
+/*int numero =4 ;
+int operacion= numero % 2;
+if(operacion == 0){
+    System.out.println("The numero is Par: "+numero);
+    }else if(operacion !=0){
+    System.out.println("The numbero is Impar: "+numero);
+}
+*/
+
+//Realiza un programa que determine si el estudiante reprueba o aprueba
+//Si la nota es mayor o igual que 90, imprima "Excelente"
+//Si es de 70 a 89 es "Aprobado"
+//Si es menor de 70 "Reprueba"
+int notaF =40 ;
+if((notaF >=0) && (notaF <= 100)){
+    if(notaF >= 90){
+    System.out.println("Tu nota es: "+notaF+", Aprobado.");
+    }else if((notaF >=70)&& (notaF<=89)){
+    System.out.println("Tu nota es: "+notaF+", Aprobado.");
+    }else if(notaF<70){
+        System.out.println("Tu nota es: "+notaF+", Reprobado.");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
